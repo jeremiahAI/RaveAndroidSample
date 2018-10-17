cac603c41a9b56b7b052b09c96a77a9347a49466
Jeremiah Ifeanyichukwu <varisiv@gmail.com>
2018-10-17 14:24:28 +0100

    Initial commit

---
+++ Real Estate/src/main/java/com/flutterwave/raveandroidsample/MainActivity.java
@@ -0,0 +1,32 @@
+package com.flutterwave.raveandroidsample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.flutterwave.raveandroid.RavePayManager;
+
+public class MainActivity extends AppCompatActivity {
+    Button payButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        payButton = findViewById(R.id.pay);
+        payButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pay();
+            }
+        });
+
+    }
+
+    private void pay() {
+        RavePayManager ravePayManager = new RavePayManager(MainActivity.this);
+        ravePayManager.setAmount(500000);
+    }
+}
---
+++ Real Estate/src/main/java/com/flutterwave/raveandroidsample/MyConstants.java
@@ -0,0 +1,10 @@
+package com.flutterwave.raveandroidsample;
+
+public class MyConstants {
+//    Todo: Add API keys
+    public static String STAGING_PUBLIC_KEY = "";
+    public static String STAGING_SECRET_KEY = "";
+    public static String LIVE_PUBLIC_KEY = "";
+    public static String LIVE_SECRET_KEY = "";
+
+}
